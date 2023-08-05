cd53f3116d1aa8c2a15742ce5cf51802a1af0ab6
meshva <5095meshwapatel@gmail.com>
2023-08-05 14:09:49 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+void main()
+{
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Center(child:Text('Hello\n \n \n\nDart\n \n \n \nFlutter')),
+    ),
+  );
+
+}
